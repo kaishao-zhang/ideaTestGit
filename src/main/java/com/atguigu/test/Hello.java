b944ac3406aa585c1ca7c971fb89c4db08706d53
zyk <1484820861@qq.com>
2019-11-16 17:41:25 +0800

    这是一个java项目

---
+++ src/main/java/com/atguigu/test/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+/**
+ * @author 凯少
+ * @create 2019-11-16 17:38
+ */
+public class Hello {
+    public  void  test(){
+        System.out.println("测试一下git托管");
+    }
+}
